c39844df5788dfdf87a584c70f91967300026bcf
Tusharbecoding <tusharchopra02122002@gmail.com>
2023-07-26 19:49:15 +0530

    final commit colored init

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+use colored::*;
 
 fn main() {
     println!("Guess the number!");
@@ -12,24 +13,26 @@ fn main() {
     loop {
         println!("Please input your guess: ");
 
+        let mut guess: String = String::new();
 
-    let mut guess: String = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+             Ok(num) => num,
+                Err(_) => continue,
+        };
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        println!("You guessed: {}", guess);
 
-    println!("You guessed: {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering :: Less => println!("Too small!"),
-        Ordering :: Greater => println!("Too big!"),
-        Ordering :: Equal => {println!("You win!");
-    break;},
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("{}", "Too small!".red()),
+            Ordering::Greater => println!("{}", "Too big!".red()),
+            Ordering::Equal => {
+                println!("{}", "You win!".green());
+                break;
+            }
+        }
     }
-    }
-    
-    
 }
